1afb6a8f8ee00b29bb7afafc6c13be6bf90d2d9a
Shamsudeen005 <shamsudeenomotosho774@gmail.com>
2023-06-16 17:01:31 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,8 +11,8 @@ for (i = 0; i <= 8; i++)
 {
 for (j = i + 1; j <= 9; j++)
 {
-putchar(i + '0');
-putchar(j + '0');
+putchar((i % 10) + '0');
+putchar((j % 10) + '0');
 if (i != 8 || j != 9)
 {
 putchar(',');
