13b28a0431dc550bea3b4820d14e28728475a7e8
Shamsudeen005 <shamsudeenomotosho774@gmail.com>
2023-06-20 03:35:44 -0700

    main updated

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
-#ifndef main.h
-#define main.h
+#ifndef main_h
+#define main_h
+ int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,3 +14,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
+#endif
