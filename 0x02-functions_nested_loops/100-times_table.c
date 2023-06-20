8a124b1c2e87280ee4a91bead3164e27954b662c
Shamsudeen005 <shamsudeenomotosho774@gmail.com>
2023-06-20 14:49:57 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,6 +7,8 @@ void print_times_table(int n)
 {
 int num, mult, prod;
 
+if (n >= 0 && n <= 15)
+{
 for (num = 0; num <= n; num++)
 {
 _putchar('0');
@@ -31,5 +33,6 @@ _putchar((prod / 10) + '0');
 _putchar((prod % 10) + '0');
 }
 _putchar('\n');
+}
 }
 }
