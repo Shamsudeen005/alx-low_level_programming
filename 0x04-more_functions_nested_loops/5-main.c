3ee691b0678e7db8978ee49f2420b2fcc6aa7f88
Shamsudeen005 <shamsudeenomotosho774@gmail.com>
2023-06-22 04:00:06 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  *main -check the code
+  *Return: 0(succss)
+  */
+
+int main(void)
+{
+more_numbers();
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  *more_numbers -To print 10 times
+  */
+void more_numbers(void)
+{
+int i;
+
+for (i = 0; i <= 10; i++)
+{
+int j;
+for (j = 0; j < 15; j++)
+{
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
+}
+_putchar('\n');
+}
+}
