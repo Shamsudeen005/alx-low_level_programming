7977eaf9e1a93b99d4ae9d424ee55c20c0465823
Shamsudeen005 <shamsudeenomotosho774@gmail.com>
2023-06-22 00:32:58 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *_isupper -To chech for the uppercase letter
+  *@c:The character to be checked
+  *Return: 1 if c is character else return 0
+  */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+  * _putchar -write the character c to unistd
+  * @c: The character to be print 
+  * Return: on success 1
+  * on error, -1 is returned and errno is set approproiately
+  */
+
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
