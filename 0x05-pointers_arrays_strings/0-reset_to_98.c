d8145f22b50ccfa68cce78524bb5b576950e9053
Shamsudeen005 <shamsudeenomotosho774@gmail.com>
2023-06-26 07:45:31 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  *reset_to_98 - Takes pointers to an int as parameters
+  *@n: integer to check
+  *Return: Return nothing
+  */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+The alx project on pointer arrays and string
