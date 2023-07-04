7cb9223bdb7d7998e84c09fefa673a9b6df2a6d9
Shamsudeen005 <shamsudeenomotosho774@gmail.com>
2023-07-04 06:53:15 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-main.c
+++ 0x07-pointers_arrays_strings/7-main.c
@@ -2,11 +2,9 @@
 #include <stdio.h>
 
 /**
-   * main - check the code
-    *
-     * Return: Always 0.
-      */
-
+ * main - check the code
+ * Return: Always 0.
+ */
 int main(void)
 {
 char board[8][8] = {
